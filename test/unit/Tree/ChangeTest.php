2ac6fcc23333c1cf89f5917fd6ede60eef54c517
Szczepan Hołyszewski <rulatir@wp.pl>
2021-02-04 14:31:38 +0100

    Complete ChangeTest after re-cloning the project to unconfuse PHPStorm

--- test/unit/Tree/ChangeTest.php
+++ test/unit/Tree/ChangeTest.php
@@ -132,16 +132,57 @@ class ChangeTest extends TestCase
 
     public function testChangeToArray()
     {
-
+        [$from, $to] = Change::fromChangeLine("R foo/bar.txt\tbaz/mek.txt");
+        $arrayFrom = Change::changeToArray($from);
+        $this->assertEquals(Change::STATUS_RENAMED_FROM, $arrayFrom['status']);
+        $this->assertEquals('foo/bar.txt', $arrayFrom['path']);
+        $this->assertEquals('baz/mek.txt', $arrayFrom['to']);
+        $arrayTo = Change::changeToArray($to);
+        $this->assertEquals(Change::STATUS_RENAMED_TO, $arrayTo['status']);
+        $this->assertEquals('baz/mek.txt', $arrayTo['path']);
+        $this->assertEquals('foo/bar.txt', $arrayTo['from']);
     }
 
-    public function testCompare()
+    public function compare() : array
     {
+        return [
+            [0, 'A foo/bar.txt', 'A foo/bar.txt'],
+            [0, 'A foo/bar.txt', 'AD foo/bar.txt'],
+            [0, 'AD foo/bar.txt', 'A foo/bar.txt'],
+            [-1, 'A foo/annie.txt', 'A foo/bar.txt'],
+            [-1, 'A foo/annie.txt', 'D foo/bar.txt'],
+            [-1, 'D foo/annie.txt', 'A foo/bar.txt'],
+            [1, 'A foo/bar.txt', 'A foo/annie.txt'],
+            [1, 'A foo/bar.txt', 'D foo/annie.txt'],
+            [1, 'D foo/bar.txt', 'A foo/annie.txt'],
+            [-1, 'A foo/bar.txt', 'D foo/bar.txt'],
+            [1, 'D foo/bar.txt', 'A foo/bar.txt'],
+        ];
+    }
 
+    /**
+     * @dataProvider compare
+     * @param int $expected
+     * @param string $lhsLine
+     * @param string $rhsLine
+     */
+    public function testCompare(int $expected, string $lhsLine, string $rhsLine)
+    {
+        $lhs = Change::fromChangeLine($lhsLine)[0];
+        $rhs = Change::fromChangeLine($rhsLine)[0];
+        self::assertEquals($expected, Change::compare($lhs, $rhs));
     }
 
     public function testToArray()
     {
-
+        [$from, $to] = Change::fromChangeLine("R foo/bar.txt\tbaz/mek.txt");
+        $arrayFrom = $from->toArray();
+        $this->assertEquals(Change::STATUS_RENAMED_FROM, $arrayFrom['status']);
+        $this->assertEquals('foo/bar.txt', $arrayFrom['path']);
+        $this->assertEquals('baz/mek.txt', $arrayFrom['to']);
+        $arrayTo = $to->toArray();
+        $this->assertEquals(Change::STATUS_RENAMED_TO, $arrayTo['status']);
+        $this->assertEquals('baz/mek.txt', $arrayTo['path']);
+        $this->assertEquals('foo/bar.txt', $arrayTo['from']);
     }
 }
